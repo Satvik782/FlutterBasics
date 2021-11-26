3598ee54eb48a0754005d54a574f4957483f03fb
Satvik782 <stvek.stvek@gmail.com>
2021-11-26 20:29:49 +0530

    lesson 8

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello Ninjas!',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-          ),
-        ),
+        child: Image.asset('assets/splash 1.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
