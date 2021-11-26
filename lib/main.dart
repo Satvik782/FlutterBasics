8f61c490bfff6f37f2bae3628a5535b24e128743
Satvik782 <stvek.stvek@gmail.com>
2021-11-26 19:55:12 +0530

    lesson 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main()=> runApp(MaterialApp(
-  home: Text('Hey Coders!')
-));
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My First App'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+          'Hello Ninjas!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower'
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child: Text('click'),
+      backgroundColor: Colors.red[600],
+    ),
+    ),
+  ));
