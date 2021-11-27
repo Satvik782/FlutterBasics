0acc940334baadb2d32467d0556116a9aadc82c9
Satvik782 <stvek.stvek@gmail.com>
2021-11-27 22:43:51 +0530

    lesson 17

---
+++ quotes/lib/quote.dart
@@ -0,0 +1,10 @@
+class Quote{
+
+  late String text;
+  late String author;
+
+  Quote({required this.text, required this.author});
+
+}
+
+// Quote myQuote = Quote(text:'this is my quote' , author:'oscar wilde');
